499a8e6b47c2036d0569c1bccc815da404913825
naoto <s1240147@u-aizu.ac.jp>
2018-05-13 17:05:32 +0900

    username

--- book/main.go
+++ book/main.go
@@ -11,6 +11,11 @@ import (
     "log"
     //"github.com/trace"
     "flag"
+    "github.com/stretchr/gomniauth"
+    "github.com/stretchr/gomniauth/providers/github"
+    "github.com/stretchr/gomniauth/providers/google"
+    "github.com/stretchr/gomniauth/providers/facebook"
+    "github.com/stretchr/objx"
 )
 
 //tmp1は１つのテンプレートを表します
@@ -26,43 +31,37 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
             template.Must(template.ParseFiles(filepath.Join("templates",
                 t.filename)))
             })
-            t.templ.Execute(w,r)
+        data := map[string]interface{}{
+            "Host": r.Host,
+        }
+        if authCookie, err := r.Cookie("auth"); err == nil {
+            data["UserData"] = objx.MustFromBase64(authCookie.Value)
+        }
+            t.templ.Execute(w,data)
     }
 
 
 func main() {
-//    http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
-//        w.Write([]byte(`
-//            <html>
-//                <head>
-//                    <title>チャット</title>
-//                    </head>
-//                    <body>
-//                    チャットしましょう
-//                    </body>
-//                    </html>
-//                    `))
-//                })
-//
-
-// /       //webサーバを開始します。
-//        if err := http.ListenAndServe(":8080",nil); err != nil {
-//            log.Fatal("ListenAndServe:",err)
- //       }
- //
- //   http.Handle("/",&templateHandler{filename: "chat.html"})
- //   //web サーバを開始します
- //   if err := http.ListenAndServe(":8080",nil); err != nil {
- //       log.Fatal("ListenAndServe:", err )
- //   }
 
  var addr = flag.String("addr",":8080","アプリケーションのアドレス")
  flag.Parse() //parse the flags
 
+
+
+ //Gomniauthのセットアップ
+ gomniauth.SetSecurityKey("セキュリティキー")
+ gomniauth.WithProviders(
+     facebook.New("クライアントID","秘密の値","http://localhost:8080/auth/callback/facebook"),
+     github.New("クライアントID","秘密の値","http://localhost:8080/auth/callback/github"),
+     google.New("652120078645-lklhekp7f29qo895aqpikitksao2p05k.apps.googleusercontent.com","KFOo-MOctyM2nCsJRXa7_RqY","http://localhost:8080/auth/callback/google"),
+ )
+
  r := newRoom()
  //r.tracer = trace.New(os.Stdout) //tracer.offをしない状態でこの一行がないと異常終了となる。
 
- http.Handle("/",&templateHandler{filename:"chat.html"})
+ http.Handle("/chat",MustAuth(&templateHandler{filename:"chat.html"}))//こうすることで、まずauthhandlerのServeHTTP-> templateHandelerのServeHTTPとなる。
+ http.Handle("/login",&templateHandler{filename:"login.html"})
+ http.HandleFunc("/auth/",loginHandler)
  http.Handle("/room",r)
 
  //get the room going
