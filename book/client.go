1047913c2c066228dc12b462732b3a13b1bdb16e
naoto <s1240147@u-aizu.ac.jp>
2018-05-13 19:57:36 +0900

    json

--- book/client.go
+++ book/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "github.com/gorilla/websocket"
+    "time"
 )
 
 //client はチャットをおこなっている１人のユーザを表します。
@@ -12,28 +13,31 @@ type client struct {
     send chan []byte
     //room はこのクライアントが参加しているチャットルーム
     room *room
+    //usreDataはユーザーに関する情報を保持します。
+    userData map[string]interface{}
 }
 
 func ( c *client ) from_client() {
     for {
-        if _, msg , err := c.socket.ReadMessage(); err == nil {
-            c.room.forward <- msg //フロントから送られて来たmsgをforwardへ送信する。
-           // fmt.Printf("%v -> %v",c.room,msg)
-        }else{
+        var msg *message
+        if err := c.socket.ReadJSON(&msg); err == nil {
+            msg.When = time.Now()
+            msg.Name = c.userData["name"].(string)
+            c.room.forward <- msg
+        } else {
+            break
+        }
+    }
+
+    c.socket.Close()
+}
+
+
+func (c *client) write() {//メッセージを書き込んでいます。 Goで実行されているために、c.sendが送信された瞬間にこのメソッドは実行されます。
+    for msg := range c.send{///フロントへ書き込んでいます。
+        if err := c.socket.WriteJSON(msg); err != nil{
             break
         }
     }
     c.socket.Close()
 }
-
-func (c *client ) write() {//メッセージを書き込んでいます。 Goで実行されているために、c.sendが送信された瞬間にこのメソッドは実行されます。
-    for msg := range c.send {
-        if err := c.socket.WriteMessage(websocket.TextMessage, msg);//フロントへ書き込んでいます。
-        err != nil {
-            break
-        }
-        //fmt.Println(msg)
-    }
-    c.socket.Close()
-}
-
